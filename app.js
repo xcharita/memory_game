82973c72ca559b517a3f33f11ca622493fd9f29a
xcharita <34929391+xcharita@users.noreply.github.com>
2019-07-16 21:47:02 -0700

    July 16

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
 let firstCard, secondCard;
 let ArrayGameOver = [];
-let NumberOfMoves = 0;
+let numberOfMoves = 0;
 let p = 0;
 let cardIsOpen = false;
 let isFirstGame = true;
+let numberOfStars = 3;
 
 var allMoves = document.querySelector('.moves');
 var allStars = document.querySelector('.stars');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -34,55 +34,33 @@ function displayCard() {
     } else {
         secondCard = this;
         secondCard.classList.add('open', 'show'); 
-        
+        cardIsOpen = false;
         if (firstCard.firstElementChild.className === secondCard.firstElementChild.className) {
             firstCard.classList.add('match');
-            secondCard.classList.add('match');
-            
-            firstCard.removeEventListener('click', displayCard);
-            secondCard.removeEventListener('click', displayCard);
-            
-            GameOver(firstCard.firstElementChild.className,secondCard.firstElementChild.className);
+            secondCard.classList.add('match');           GameOver(firstCard.firstElementChild.className,secondCard.firstElementChild.className);
             
         } else {
                 setTimeout(function() {
                     isClosed(firstCard);
                     isClosed(secondCard);
                 }, 500);
-           
+          }
         }   
-        cardIsOpen = false;
-    }
-    NumberOfMoves++;
-    console.log(NumberOfMoves);
-    allMoves.textContent = NumberOfMoves;
-    
+    numberOfMoves++;
+    allMoves.textContent = numberOfMoves;
+    starRating();
 };
 
-function DisplayStars(NumberOfMoves) {
-    /*    
-        	<ul class="stars">
-        		<li><i class="fa fa-star"></i></li>
-        		<li><i class="fa fa-star"></i></li>
-        		<li><i class="fa fa-star"></i></li>
-        	</ul>
-*/
-let howManyStars = 0;
-let newStars;
-var element  = document.getElementsByClassName('stars');
-var fragment = document.createDocumentFragment();
-    
-
-console.log(NumberOfMoves);
-howManyStars = (NumberOfMoves%3);
-newStars.innerHTML = [];    
-console.log(howManyStars);
-    for (i=0; i < howManyStars; i++) {
-            newStars = document.createElement('li');
-            newStars.innerHTML = '<i class="fa fa-star"></i>';
-            fragment.appendChild(newStars);
-    }   
-    element.appendChild(fragment);
+const stars = document.querySelectorAll('.stars i');
+function starRating(){
+    if (numberOfMoves > 30){
+        stars[1].classList.remove('fa-star');
+        numberOfStars = 1;
+    }
+    else if (numberOfMoves > 20){
+        stars[2].classList.remove('fa-star');
+        numberOfStars = 2;
+    }
 };
 
 function isClosed(card) {
@@ -93,74 +71,79 @@ function newGame() {
     let cardsForNewGame = document.querySelectorAll('.card');
     let newCardsArray = [];  
     let ArrayGameOver = [];
-    NumberOfMoves = 0;
-    console.log(NumberOfMoves);
-    allMoves.textContent = NumberOfMoves;
-    for (let i=0; i< cardsForNewGame.length; i++) {
-        newCardsArray.push(cardsForNewGame[i].children[0].className);
-    };
+    time = 0;
+    numberOfMoves = 0;
+    allMoves.textContent = numberOfMoves;
+    stars[1].classList.add('fa-star');
+    stars[2].classList.add('fa-star');
+    startTimerFunction();
+// push all the cards in an array to make ready for the shuffle function    
+        for (let i=0; i< cardsForNewGame.length; i++) {
+            newCardsArray.push(cardsForNewGame[i].children[0].className);
+        };
     setNewCards = shuffle(newCardsArray);
 // Loop through the NodeList and assign the correct className to the NodeList
-    for (var i = 0; i < cardsForNewGame.length; i++) {
-        cardsForNewGame[i].className = "card";
-        cardsForNewGame[i].firstElementChild.className = setNewCards[i];
-    };
-    cardsForNewGame.forEach(function(card){
-            card.addEventListener('click', displayCard) });
+        for (var i = 0; i < cardsForNewGame.length; i++) {
+            cardsForNewGame[i].className = "card";
+            cardsForNewGame[i].firstElementChild.className = setNewCards[i];
+        };
+    cardsForNewGame.forEach(function(newCard){
+            newCard.addEventListener('click', displayCard) });
 };
 
 function GameOver(firstcard, secondcard) {
     ArrayGameOver.push(firstcard);
     ArrayGameOver.push(secondcard);
-    console.log(ArrayGameOver);
-    console.log(p);
     if (!isFirstGame) {
         if (ArrayGameOver.length === allCards.length) {
+            stopTimerFunction();
             window.alert("Game Over !!");
-            //stopTimer;
+            DisplayResults();
         }
     } else {
-            if (ArrayGameOver.length === (allCards.length-2)) {
-                window.alert("Game Over !!");
-                //stopTimer; 
+            if (ArrayGameOver.length === (allCards.length)) {
+                endGame = true;
+                stopTimerFunction(); 
+                DisplayResults();
             }
     }
-    console.log(firstcard);
-    console.log(secondcard);
-    p++;
-    DisplayStars(p);
 };
 
-/*
-function startTimer() {
-    var second = 0, minute = 0; hour = 0;
-    var timer = document.querySelector(".timer");
-    var interval;
-    interval = setInterval(function() {
-        timer.textContent = minute+"mins "+second+"secs";
-        second++;
-        if(second == 60){
-            minute++;
-            second=0;
-        }
-        if(minute == 60){
-            hour++;
-            minute = 0;
-        }
-    },1000);
+const yourTime = document.querySelector('.timer');
+
+let t;
+let time = 0;
+function pad (val) { 
+    return val > 9 ? val : "0" + val; 
 };
 
-function stopTimer() {
-    //alertbox "you have finished the game in blalaa" secods
-    
+function gameTimer() {
+    t = setTimeout(gameTimer, 1000);
+    ++time;
+    let minute = pad(Math.floor(time / 60));
+    let seconds = pad(time - (minute * 60));
+    yourTime.innerHTML = `${minute}:${seconds}`;
 };
-*/
+
+function startTimerFunction() {
+    if(time == 0){
+        gameTimer();
+    } 
+};
+
+function stopTimerFunction() {
+        clearTimeout(t);
+};
+
 var allCards = document.querySelectorAll('.card');
 var shuffleOption = document.querySelector('.restart');
+const startButton = document.querySelector('.start-button');
+const stopButton = document.querySelector('.stop-button');
 
-
-//startTimer();
 allMoves.textContent = 0;
+startButton.addEventListener('click', startTimerFunction);
+stopButton.addEventListener('click', stopTimerFunction);
+
 shuffleOption.addEventListener('click', newGame);
 allCards.forEach(function(card){
             card.addEventListener('click', displayCard); 
